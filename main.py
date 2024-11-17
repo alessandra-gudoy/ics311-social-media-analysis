c3a0830f92034bf5a051ba92cb68f76dbc7e263c
alessandraGudoy <gudoyalessandra@gmail.com>
2024-11-17 05:24:32 -1000

    added comments and connections to users

--- main.py
+++ main.py
@@ -8,6 +8,26 @@ bob = User("bob", [], [], [], [])
 peter = User("peter", [], [], [], [])
 sarah = User("sarah", [], [], [], [])
 
+# connections
+john.add_connection(Connection(john, alice, ConnectionType.FRIEND))
+john.add_connection(Connection(john, bob, ConnectionType.COWORKER))
+john.add_connection(Connection(john, peter, ConnectionType.FOLLOWER))
+john.add_connection(Connection(john, sarah, ConnectionType.HAS_READ_POSTS_BY))
+
+alice.add_connection(Connection(alice, john, ConnectionType.FRIEND))
+alice.add_connection(Connection(alice, peter, ConnectionType.COWORKER))
+alice.add_connection(Connection(alice, sarah, ConnectionType.FRIEND))
+
+bob.add_connection(Connection(bob, john, ConnectionType.COWORKER))
+bob.add_connection(Connection(bob, sarah, ConnectionType.FRIEND))
+
+peter.add_connection(Connection(peter, alice, ConnectionType.COWORKER))
+peter.add_connection(Connection(peter, sarah, ConnectionType.HAS_READ_POSTS_BY))
+
+sarah.add_connection(Connection(sarah, john, ConnectionType.HAS_READ_POSTS_BY))
+sarah.add_connection(Connection(sarah, alice, ConnectionType.FRIEND))
+sarah.add_connection(Connection(sarah, bob, ConnectionType.FRIEND))
+
 network = Network([john, alice, bob, peter, sarah])
 
 # make comments
@@ -43,7 +63,7 @@ posts["alice"][1].add_viewer(generate_view(sarah, posts["alice"][1]))
 
 posts["peter"][0].add_viewer(generate_view(john, posts["peter"][0]))
 posts["peter"][0].add_viewer(generate_view(alice, posts["peter"][0]))
-posts["peter"][0].add_viewer(generate_view(bob, posts["peter"][0]))
+posts["peter"][0].add_viewer(generate_view(sarah, posts["peter"][0]))
 
 posts["peter"][1].add_viewer(generate_view(sarah, posts["peter"][1]))
 
@@ -53,10 +73,22 @@ posts["sarah"][0].add_viewer(generate_view(bob, posts["sarah"][0]))
 posts["sarah"][0].add_viewer(generate_view(peter, posts["sarah"][0]))
 
 for user in posts.items():
-    posts = user[1]
-    for post in posts:
+    for post in user[1]:
         for viewer in post.get_viewers():
             viewer.get_who_viewed().add_viewed_post(post)
+
+# add comments to posts
+posts["alice"][0].add_comment(comments["john"][0])
+posts["peter"][0].add_comment(comments["john"][1])
+posts["sarah"][0].add_comment(comments["john"][2])
+
+posts["john"][0].add_comment(comments["bob"][0])
+posts["sarah"][0].add_comment(comments["bob"][1])
+
+posts["alice"][1].add_comment(comments["peter"][0])
+
+posts["john"][0].add_comment(comments["sarah"][0])
+posts["alice"][1].add_comment(comments["sarah"][1])
 
 
 network.print_users()
