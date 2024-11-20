0c79cb37a93197cb1584904502deb10271672261
Mhar Olipani <mhar@hawaii.edu>
2024-11-19 23:44:26 -1000

    Mhar Commit
    Completed Implementation of Interesting Posts.

--- intposts_analysis.py
+++ intposts_analysis.py
@@ -14,18 +14,21 @@ def bfs(G, start_node):
     
     return order
 
-def find_important_posts(G, importance_criteria='comments'):
+def find_important_posts(G, importance_criteria='comments', threshold=1):
     pq = PriorityQueue()
 
     for node, data in G.nodes(data=True):
         if data['type'] == 'post':
+            priority = 0
             if importance_criteria == 'comments':
                 priority = len(node.get_comments())
             elif importance_criteria == 'views':
                 priority = len(node.get_viewers())
             else:
                 priority = len(node.get_comments()) + len(node.get_viewers())
-            pq.put((-priority, node.get_time(), node))  # The priority queue uses negative priority for max-heap behavior
+            
+            if priority >= threshold:
+                pq.put((-priority, node.get_time(), node))  # The priority queue uses negative priority for max-heap behavior
 
     important_posts = []
     while not pq.empty():
